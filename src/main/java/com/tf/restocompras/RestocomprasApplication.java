c1ccf50e3e6c93697cd5e67f1530fd4f3d727d10
Tomas Fontanarrosa <tomas.fontanarrosa@edreamsodigeo.com>
2024-07-22 19:48:44 +0200

    Make Resto Great Again

---
+++ src/main/java/com/tf/restocompras/RestocomprasApplication.java
@@ -0,0 +1,13 @@
+package com.tf.restocompras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestocomprasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestocomprasApplication.class, args);
+	}
+
+}
