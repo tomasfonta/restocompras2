123574d1827b02b70223b78f4e60b286b2869b7d
tomas fontanarrosa <tomasfontanarrosa@tomass-MacBook-Air.local>
2025-10-01 19:12:45 +0200

    Update cors config

--- src/main/java/com/tf/restocompras/config/security/CorsConfig.java
+++ src/main/java/com/tf/restocompras/config/security/CorsConfig.java
@@ -13,13 +13,17 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "https://restocompras-prod-front.onrender.com")
+                        .allowedOrigins(
+                                "http://localhost:3000",
+                                "http://127.0.0.1:3000",
+                                "https://restocompras-prod-front.onrender.com",
+                                "http://restocompras-prod-front.onrender.com"
+                        )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
-                        .exposedHeaders("*") // Allow all headers to be exposed
+                        .exposedHeaders("*")
                         .allowCredentials(true);
             }
         };
     }
 }
-
